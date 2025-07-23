ab0808ea5a1e653276be13cab46d0144790c229a
fangying88 <jjr9874tw@gmail.com>
2025-07-23 17:09:35 +0800

    0723_2

---
+++ index.js
@@ -0,0 +1,68 @@
+console.log("hello world")
+0.5
+1
+2
+;("use strict")
+;("jhhjgj")
+true
+false
+
+// let number = 2
+// number = 3
+// let temp = number
+// console.log(temp)
+// number = 4
+// temp = number
+// console.log(temp) 
+
+// let a = 10
+// let b = 20
+// let c
+// c=a
+// a=b
+// b=c
+// console.log(a,b)
+
+// && || == !=
+
+let a = 10
+let b = 20
+// 邏輯運算
+// console.log(a==10)
+// console.log(a!=10)
+// console.log(a<=10&&b>=20)
+// console.log(a==10||b==10)
+
+// 算數
+// console.log(1+2)
+// console.log(2-1)
+// console.log(2*3)
+// console.log(6/3)
+// console.log(5%2)
+
+// if(a == 10){
+//     console.log("a is 10")
+// }else if(a == 15){
+//     console.log("a is 15")
+// }else{
+//     console.log("a is not equal 10 and 15")
+// }
+
+// for(let i=0;i<10;i++){
+//     console.log(i)
+// }
+// console.log("for loop end")
+
+// let double = function(num1,num2){
+//     return num1* 2+num2 * 2
+// }
+// let c = double(2,3)
+// console.log(c)
+
+// let myname = function(name){}
+let all =0
+for(let i =1;i<=100;i++){
+    // console.log(i)
+    all=all+i
+}
+console.log(all)
